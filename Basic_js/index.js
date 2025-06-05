2b192bb20c1defb1e5ebddb65df82b4115bc115d
ankitNegiDev <ankitnegi9104@gmail.com>
2025-06-05 06:40:59 +0000

    variable and data types

--- Basic_js/index.js
+++ Basic_js/index.js
@@ -1,1 +1,67 @@
 console.log("hello this is starting of js revision");
+// Basic --- js --- starting ----------------------
+
+/**
+ * js is for all the logical and interectivity part of a web-page (web-site)
+ * js is programming language and it has feature like -- (capability of descision, task reptions, we can build our own logic, we can do computation and so on.)
+ * thre are three way to execute js ---> (1) directly using browser console , (2) using node , (3) using html and linking js file and all output will be shown in browser console.
+ * programme -> a file where we write our logical piece of code
+ * process -> programm under execution is called process. whenever we run any program in our machine that become the process..
+*/
+
+// variable -> variable are used to store data during execution of program -- we can crate any number of varaible and these variable will be uniquely identified based on there name and using this varaible name we can  fetch , update and store data in the varaibles.
+
+// inorder to create varaible we have let ,var,const..
+
+// console is a object and log() is a function which is a property of a console object or we can sya its method of console object.
+let variableCreatedUsingLet="bingolive";
+console.log("let varaible is : ",variableCreatedUsingLet);
+const variableCreatedUsingConst="secret-key";
+console.log("const varaible is : ",variableCreatedUsingConst);
+
+var variableCreatedUsingVar="some-variable-name";
+console.log("var varaible is : ",variableCreatedUsingVar);
+
+// varaible naming rule
+/**
+ * a varaible name can contain letter,digit,_ and $
+ * variable name can start with letter,_ and $
+ * varaible name are case sensitive
+ * reserved word can't be variable name..
+ * by default in js we use camelCase..
+ */
+let a=10;
+let A=100; // case sensitive a and A are diff
+let _my="lii"; // valid
+let $_so="ii"; // valid
+// let 8age="some"  // error varaible start with digit..
+// let if="some" // error - keyword used
+
+// Data types -- primitive  and non-primitev
+
+// number
+let age=23;
+console.log("age is : ",age);
+
+// string
+let name="bingo";
+console.log("name is : ",name);
+
+// boolean
+let bool=true;
+console.log("bool is : ",bool);
+
+// null
+let currentDay=null;
+console.log("currentDay is : ",currentDay);
+
+// undefined
+let mars;
+console.log("mars is : ",mars);
+
+// symbol
+
+// bigint -- n at the last of value.
+
+
+
